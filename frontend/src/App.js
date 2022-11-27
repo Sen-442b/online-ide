df747665e689efbf7f7a0ecac56ddf5a054b8b85
Aryan Sarang <thearyansarang@gmail.com>
2022-11-27 18:36:57 +0530

    frontend upto refrence video

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,132 @@
-import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
+import React, { useState, useEffect } from "react";
+import stubs from "./defaultStubs";
+import moment from 'moment';
 
 function App() {
+
+  const [code, setcode] = useState("");
+  const [output, setOutput] = useState("");
+  const [language, setLanguage] = useState("cpp");
+  const [status, setStatus] = useState("");
+  const [jobId, setJobId] = useState("");
+  const [jobDetails, setJobDetails] = useState(null);
+
+  useEffect(() => {
+    const defaultLang = localStorage.getItem("default-language") || "cpp"
+    setLanguage(defaultLang);
+  },[]);
+
+  useEffect(() => {
+    setcode(stubs[language]);
+  }, [language]);
+
+  const setDefaultLanguage =()=>{
+    localStorage.setItem("default-language",language);
+    console.log(`${language} set as default language.`)
+  }
+
+  const renderTimeDetails = ()=>{
+    if(!jobDetails){
+      return "";
+    }
+    let result = '';
+    let {submittedAt, completedAt, startedAt}= jobDetails;
+    submittedAt = moment(submittedAt).toString()
+    result += `Submitted At: ${submittedAt}`
+    if(!completedAt || !startedAt){
+      return result; 
+    }
+    const start = moment(startedAt);
+    const end= moment(completedAt);
+    const executionTime = end.diff(start,'seconds', true);
+    result += `Execution Time: ${executionTime}s`
+    return result;
+  }
+  const handleSubmit = async () => {
+    const payload = {
+      language,
+      code,
+    };
+    try {
+      setJobId("");
+      setStatus("");
+      setOutput("");
+      setJobDetails(null);
+      const { data } = await axios.post("http://localhost:5000/run", payload);
+      console.log(data);
+      setJobId(data.jobId);
+      let intervalId = setInterval(async () => {
+        const { data: dataRes } = await axios.get("http://localhost:5000/status",
+          { params: { id: data.jobId } }
+        );
+        const { success, job, error } = dataRes;
+
+        if (success) {
+          const { status: jobStatus, output: jobOutput } = job;
+          setStatus(jobStatus);
+          setJobDetails(job);
+          if (jobStatus === "pending") return;
+          setOutput(jobOutput);
+          clearInterval(intervalId);
+
+        } else {
+          setStatus("Error: Please retry!");
+          console.error(error);
+          clearInterval(intervalId);
+          setOutput(error);
+        }
+        console.log(dataRes);
+      }, 1000);
+
+    } catch ({ response }) {
+      if (response) {
+        const errMsg = response.data.err.stderr;
+        setOutput(errMsg);
+      } else {
+        setOutput("Error connecting to server");
+      }
+    }
+  };
+  
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Online Code Editor</h1>
+      <div>
+        <label>Language: </label>
+        <select value={language} onChange={(e) => {
+          let response = window.confirm(
+            "WARNING: Switching the language, will remove your current code. Do you wish to proceed?"
+          )
+          if(response)
+          {
+            setLanguage(e.target.value);
+          }
+        }}>
+          <option value="cpp">C++</option>
+          <option value="py">Python</option>
+          <option value="js">Javascript</option>
+        </select>
+      </div>
+      <br/>
+      <div>
+      <button onClick={setDefaultLanguage}>Set Default</button>
+      </div>
+      <br />
+      <textarea rows="20" cols="75" value={code}
+        onChange={(e) => { setcode(e.target.value); }}></textarea>
+      <br />
+      <button onClick={handleSubmit}>submit</button>
+
+      <p>{status}</p>
+      <p>{jobId && `jobID: ${jobId}`}</p>
+      <p>{renderTimeDetails}</p>
+      <p>{output}</p>
     </div>
+
   );
 }
 
